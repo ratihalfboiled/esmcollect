ee8543ebcca03abbab77a120da2fa23ee05e0e02
Ratih Cahyaningtyas <ratihc97@gmail.com>
2018-05-09 09:45:53 +0700

    codeigniter 2

---
+++ application/controllers/Esm_controller.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Esm_controller extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('v_dashboard');
+	}
+}
